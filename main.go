5b37d3672de65eaf39f3fe172c69df8e3529f5ff
Conor Hayes <conchubhair@localhost.localdomain>
2020-03-08 15:50:30 +0000

    Adding server test function, cleaned up Dockerfile and main.go a little

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ import (
     "time"
 )
 
-func test(w http.ResponseWriter, r *http.Request) {
+func greeting(w http.ResponseWriter, r *http.Request) {
     dt := time.Now()
     fmt.Fprintf(w, "Hello! Welcome to my containerized web server in Golang!\nToday's date and time is: %s", dt.Format("02-01-2006 15:04:05 Monday"))
 }
 
 func main() {
     fmt.Println("Server started")
-    http.HandleFunc("/", test)
+    http.HandleFunc("/", greeting)
 
     http.ListenAndServe(":8080", nil)
 }
---
+++ main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "net/http"
+    "net/http/httptest"
+    "testing"
+    "time"
+    "fmt"
+)
+
+func TestHealthCheck(t *testing.T) {
+    req, err := http.NewRequest("GET", "/", nil)
+
+    if err != nil {
+        t.Errorf("An error occurred. %v", err)
+    }
+
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(greeting)
+
+    handler.ServeHTTP(rr, req)
+
+    if status := rr.Code; status != http.StatusOK {
+        t.Errorf("Handler returned wrong status code. Got %v, want %v", status, http.StatusOK)
+    }
+
+
+    dt := time.Now()
+    expected := fmt.Sprintf("Hello! Welcome to my containerized web server in Golang!\nToday's date and time is: %s", dt.Format("02-01-2006 15:04:05 Monday"))
+    if rr.Body.String() != expected {
+        t.Errorf("Handler returned incorrect body: Got %v, want %v", rr.Body.String(), expected)
+    }
+}
